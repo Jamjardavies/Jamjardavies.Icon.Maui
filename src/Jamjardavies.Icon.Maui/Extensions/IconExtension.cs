5f7d40a7fa87d2b64b81c83893ec63e5e83418b6
Jamjardavies <jamjar@ilsoft.co.uk>
2024-05-10 22:54:32 +0900

    Update to allow changing image source values, as well as allowing any control to be used.

--- src/Jamjardavies.Icon.Maui/Extensions/IconExtension.cs
+++ src/Jamjardavies.Icon.Maui/Extensions/IconExtension.cs
@@ -2,14 +2,31 @@
 //      Copyright (c) 2024 Jamjardavies.
 // </copyright>
 
+using System.ComponentModel;
+using System.Reflection;
+using Microsoft.Maui.Graphics.Converters;
+
 namespace Jamjardavies.Icon.Maui;
 
 [ContentProperty(nameof(Icon))]
 public abstract class IconExtension<TEnum> : IMarkupExtension where TEnum : Enum
 {
+    private static readonly Dictionary<Type, BindableType> BindableTypeMap = new()
+    {
+        { typeof(Enum), BindableType.Enum },
+        { typeof(string), BindableType.String },
+        { typeof(ImageSource), BindableType.ImageSource }
+    };
+
     #region Properties
 
     public TEnum? Icon { get; set; }
+
+    [TypeConverter(typeof(ColorTypeConverter))]
+    public Color IconColor { get; set; } = Colors.White;
+
+    [TypeConverter(typeof(FontSizeConverter))]
+    public double IconSize { get; set; } = 32;
 
     #endregion
 
@@ -25,20 +42,27 @@ public abstract class IconExtension<TEnum> : IMarkupExtension where TEnum : Enum
         IProvideValueTarget valueProvider = serviceProvider.GetService<IProvideValueTarget>()
                                          ?? throw new ArgumentException("Unable to get IProvideValueTarget service.");
 
+        if (this.Icon is null)
+        {
+            return null;
+        }
+
         if (valueProvider.TargetProperty is not BindableProperty bindableProperty)
         {
             return null;
         }
 
-        Type returnType = bindableProperty.ReturnType;
-
-        return valueProvider.TargetObject switch
+        if (!BindableTypeMap.TryGetValue(bindableProperty.ReturnType, out BindableType type))
         {
-            IconLabel => this.Icon,
-            IconButton button => this.Icon,
-            Label label => this.PopulateLabel(label),
-            Button button => this.PopulateButton(button, returnType),
-            _ => this.Icon
+            return null;
+        }
+
+        return type switch
+        {
+            BindableType.Enum => this.Icon,
+            BindableType.String => this.PopulateString(valueProvider.TargetObject),
+            BindableType.ImageSource => this.Icon.ToIconSource(this.IconColor, this.IconSize),
+            _ => throw new InvalidOperationException()
         };
     }
 
@@ -48,40 +72,35 @@ public abstract class IconExtension<TEnum> : IMarkupExtension where TEnum : Enum
 
     #region Private
 
-    private string PopulateLabel(Label label)
+    private string PopulateString(object targetObject)
     {
         if (this.Icon is null)
         {
-            throw new NullReferenceException("Icon must be set!");
+            return string.Empty;
         }
 
-        label.FontFamily = this.Icon.ToFontFamily();
+        PropertyInfo? fontFamilyProp = targetObject.GetType().GetProperty("FontFamily");
+
+        if (fontFamilyProp is not null && fontFamilyProp.CanWrite)
+        {
+            fontFamilyProp.SetValue(targetObject, this.Icon.ToFontFamily());
+        }
 
         return this.Icon.ToIconGlyph();
     }
 
-    private object? PopulateButton(Button button, Type returnType)
-    {
-        if (this.Icon is null)
-        {
-            return null;
-        }
-
-        if (returnType == typeof(ImageSource))
-        {
-            return this.Icon.ToIconSource(button.TextColor, button.FontSize);
-        }
-
-        if (returnType == typeof(string))
-        {
-            button.FontFamily = this.Icon.ToFontFamily();
-            return this.Icon.ToIconGlyph();
-        }
-
-        return null;
-    }
+    #endregion
 
     #endregion
+
+    #region Nested type: BindableType
+
+    private enum BindableType
+    {
+        Enum,
+        String,
+        ImageSource
+    }
 
     #endregion
 }
